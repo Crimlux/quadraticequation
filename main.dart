029f858655253993807c7333ade45af22f88ed35
unknown <crimlux>
2024-03-18 11:28:27 +0100

    first commit

---
+++ main.dart
@@ -0,0 +1,21 @@
+import 'dart:math';
+
+void main() {
+  double a = 1;
+  double b = -3;
+  double c = 2;
+
+  double discriminant = b * b - 4 * a * c;
+
+  if (discriminant > 0) {
+    double root1 = (-b + sqrt(discriminant)) / (2 * a);
+    double root2 = (-b - sqrt(discriminant)) / (2 * a);
+    print('Root 1: $root1');
+    print('Root 2: $root2');
+  } else if (discriminant == 0) {
+    double root = -b / (2 * a);
+    print('Repeated Root: $root');
+  } else {
+    print('No real roots');
+  }
+}
